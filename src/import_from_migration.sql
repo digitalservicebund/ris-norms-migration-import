913d6767f840af74abcc94b7e2ad85ffe8478a97
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2024-10-10 08:27:50 +0200

    Keep our seeds in the database
    Just for now
    
    RISDEV-4929

--- src/import_from_migration.sql
+++ src/import_from_migration.sql
@@ -1,5 +1,14 @@
-DELETE FROM :NORMS_SCHEMA.announcements;
-DELETE FROM :NORMS_SCHEMA.norms;
+DELETE FROM norms.announcements;
+DELETE FROM norms.norms WHERE
+    -- keep our seeds, for now
+    eli_manifestation NOT IN (
+        'eli/bund/bgbl-1/1964/s593/1964-08-05/1/deu/1964-08-05/regelungstext-1.xml',
+        'eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/1990-12-20/regelungstext-1.xml',
+        'eli/bund/bgbl-1/1000/1/1000-01-01/1/deu/1000-01-01/regelungstext-1.xml',
+        'eli/bund/bgbl-1/2009/s3366/2023-12-23/1/deu/2023-12-23/regelungstext-1.xml',
+        'eli/bund/bgbl-1/1001/1/1001-01-01/1/deu/1001-01-01/regelungstext-1.xml',
+        'eli/bund/bgbl-1/1002/1/1002-01-01/1/deu/1002-01-01/regelungstext-1.xml'
+    );
 
 INSERT INTO :NORMS_SCHEMA.norms (xml) SELECT ldml_xml.content FROM :MIGRATION_SCHEMA.migration_record
          INNER JOIN :MIGRATION_SCHEMA.ldml ldml ON migration_record.id = ldml.migration_record_id
@@ -7,5 +16,5 @@ INSERT INTO :NORMS_SCHEMA.norms (xml) SELECT ldml_xml.content FROM :MIGRATION_SC
          LEFT OUTER JOIN :MIGRATION_SCHEMA.migration_error migration_error on migration_record.id = migration_error.migration_record_id
          WHERE
              migration_status = 'LEGALDOCML_TRANSFORMATION_SUCCEEDED'
-            AND xpath_exists('//akn:act[@name="regelungstext"]', ldml_xml.content, '{{akn,http://Inhaltsdaten.LegalDocML.de/1.7/}}')
+            AND xpath_exists('//akn:act[@name='regelungstext']', ldml_xml.content, '{{akn,http://Inhaltsdaten.LegalDocML.de/1.7/}}')
             AND migration_error.id IS NULL;
