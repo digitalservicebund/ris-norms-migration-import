35e94d26c75d4c5fdbc027288431821f30ad0ddc
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2024-10-10 14:33:51 +0200

    Fix typo
    RISDEV-4929

--- src/import_from_migration.sql
+++ src/import_from_migration.sql
@@ -16,5 +16,5 @@ INSERT INTO :NORMS_SCHEMA.norms (xml) SELECT ldml_xml.content FROM :MIGRATION_SC
          LEFT OUTER JOIN :MIGRATION_SCHEMA.migration_error migration_error on migration_record.id = migration_error.migration_record_id
          WHERE
              migration_status = 'LEGALDOCML_TRANSFORMATION_SUCCEEDED'
-            AND xpath_exists('//akn:act[@name='regelungstext']', ldml_xml.content, '{{akn,http://Inhaltsdaten.LegalDocML.de/1.7/}}')
+            AND xpath_exists('//akn:act[@name="regelungstext"]', ldml_xml.content, '{{akn,http://Inhaltsdaten.LegalDocML.de/1.7/}}')
             AND migration_error.id IS NULL;
