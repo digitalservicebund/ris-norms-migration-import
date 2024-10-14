23153fa6106c8546a49543ad6c475de3d2d82590
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2024-10-14 11:38:05 +0200

    Ignore two schematron violations
    SCH-00200-005 and SCH-00210-005, they are both classified as warnings for existing norms.
    
    RISDEV-0000

--- src/import_from_migration.sql
+++ src/import_from_migration.sql
@@ -13,7 +13,11 @@ DELETE FROM :NORMS_SCHEMA.norms WHERE
 INSERT INTO :NORMS_SCHEMA.norms (xml) SELECT ldml_xml.content FROM :MIGRATION_SCHEMA.migration_record
          INNER JOIN :MIGRATION_SCHEMA.ldml ldml ON migration_record.id = ldml.migration_record_id
          INNER JOIN :MIGRATION_SCHEMA.ldml_xml ldml_xml ON ldml.id = ldml_xml.ldml_id
-         LEFT OUTER JOIN :MIGRATION_SCHEMA.migration_error migration_error on migration_record.id = migration_error.migration_record_id
+         LEFT OUTER JOIN :MIGRATION_SCHEMA.migration_error migration_error on
+            migration_record.id = migration_error.migration_record_id
+                -- ignore violations of schematron rules SCH-00200-005 and SCH-00210-005 for existing norms
+                AND migration_error.description NOT LIKE '%SCH-00210-005%'
+                AND migration_error.description NOT LIKE '%SCH-00200-005%'
          WHERE
              migration_status = 'LEGALDOCML_TRANSFORMATION_SUCCEEDED'
             AND xpath_exists('//akn:act[@name="regelungstext"]', ldml_xml.content, '{{akn,http://Inhaltsdaten.LegalDocML.de/1.7/}}')
