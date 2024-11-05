a73eb0b5f35dd4bb8d1616bf3cf391cfe772909f
VictorDelCampo <victor.delcampo@digitalservice.bund.de>
2024-11-05 15:02:43 +0100

    Create log entry | Add queued_for_publish
    RISDEV-0000

--- src/import_from_migration.sql
+++ src/import_from_migration.sql
@@ -13,16 +13,26 @@ DELETE FROM :NORMS_SCHEMA.norms WHERE
         'eli/bund/bgbl-1/1002/1/1002-01-01/1/deu/1002-01-01/regelungstext-1.xml'
     );
 
-INSERT INTO :NORMS_SCHEMA.norms (xml) SELECT ldml_xml.content FROM :MIGRATION_SCHEMA.migration_record
-         INNER JOIN :MIGRATION_SCHEMA.ldml ldml ON migration_record.id = ldml.migration_record_id
-         INNER JOIN :MIGRATION_SCHEMA.ldml_xml ldml_xml ON ldml.id = ldml_xml.ldml_id
-         LEFT OUTER JOIN :MIGRATION_SCHEMA.migration_error migration_error on
-            migration_record.id = migration_error.migration_record_id
-                -- ignore violations of schematron rules SCH-00200-005 and SCH-00210-005 for existing norms
-                AND migration_error.description NOT LIKE '%SCH-00210-005%'
-                AND migration_error.description NOT LIKE '%SCH-00200-005%'
-         WHERE
-            migration_status IN ('LEGALDOCML_TRANSFORMATION_SUCCEEDED', 'LEGALDOCML_VALIDATION_FAILED')
-            AND xpath_exists('//akn:act[@name="regelungstext"]', ldml_xml.content, '{{akn,http://Inhaltsdaten.LegalDocML.de/1.7/}}')
-            AND migration_error.id IS NULL
-    ON CONFLICT DO NOTHING;
+-- Insert into norms table and count the rows inserted using CTE
+WITH inserted_rows AS (
+INSERT INTO :NORMS_SCHEMA.norms (xml, publish_state)
+SELECT ldml_xml.content, 'QUEUED_FOR_PUBLISH'::norm_publish_state
+FROM :MIGRATION_SCHEMA.migration_record
+    INNER JOIN :MIGRATION_SCHEMA.ldml ldml ON migration_record.id = ldml.migration_record_id
+    INNER JOIN :MIGRATION_SCHEMA.ldml_xml ldml_xml ON ldml.id = ldml_xml.ldml_id
+    LEFT OUTER JOIN :MIGRATION_SCHEMA.migration_error migration_error ON
+    migration_record.id = migration_error.migration_record_id
+    AND migration_error.description NOT LIKE '%SCH-00210-005%'
+    AND migration_error.description NOT LIKE '%SCH-00200-005%'
+WHERE
+    migration_status IN ('LEGALDOCML_TRANSFORMATION_SUCCEEDED', 'LEGALDOCML_VALIDATION_FAILED')
+  AND xpath_exists('//akn:act[@name="regelungstext"]', ldml_xml.content, '{{akn,http://Inhaltsdaten.LegalDocML.de/1.7/}}')
+  AND migration_error.id IS NULL
+  ON CONFLICT DO NOTHING -- ensures duplicates are ignored
+    RETURNING *
+)
+
+-- Insert into migration_log the number of rows inserted using the CTE
+INSERT INTO :NORMS_SCHEMA.migration_log (size)
+SELECT COUNT(*) FROM inserted_rows;
+
