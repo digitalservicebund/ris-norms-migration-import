6c75f0a49f4c633a067159a90443527d274a4982
VictorDelCampo <victor.delcampo@digitalservice.bund.de>
2025-04-03 09:55:57 +0200

    Check if migration team job is still running
    RISDEV-0000

--- src/import_from_migration.sh
+++ src/import_from_migration.sh
@@ -8,4 +8,12 @@ export PGPASSWORD="${PGPASSWORD:=$(cat /etc/secrets/database-credentials/passwor
 echo "PGUSER=$PGUSER"
 echo "MIGRATION_STATS_SCHEMA=$MIGRATION_STATS_SCHEMA"
 
-psql --echo-all --variable=NORMS_SCHEMA=$NORMS_SCHEMA --variable=MIGRATION_SCHEMA=$MIGRATION_SCHEMA --file=./import_from_migration.sql
+# Check if there are any jobs in progress
+JOBS_IN_PROGRESS=$(psql --tuples-only --csv --command="SELECT COUNT(*) FROM $MIGRATION_SCHEMA.job_in_progress")
+
+if [ "$JOBS_IN_PROGRESS" -eq 0 ]; then
+  echo "No jobs in progress. Running import_from_migration.sql..."
+  psql --echo-all --variable=NORMS_SCHEMA=$NORMS_SCHEMA --variable=MIGRATION_SCHEMA=$MIGRATION_SCHEMA --file=./import_from_migration.sql
+else
+  echo "Jobs are still in progress. Skipping running norms migration job."
+fi
