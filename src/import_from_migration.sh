caaf0e8cb40a59f1889a463b9743cf646ce19d07
VictorDelCampo <victor.delcampo@digitalservice.bund.de>
2025-04-07 08:57:03 +0200

    Fix sh script
    RISDEV-0000

--- src/import_from_migration.sh
+++ src/import_from_migration.sh
@@ -9,10 +9,12 @@ echo "PGUSER=$PGUSER"
 echo "MIGRATION_STATS_SCHEMA=$MIGRATION_STATS_SCHEMA"
 
 # Check if there are any jobs in progress
-JOBS_IN_PROGRESS=$(psql --tuples-only --csv --command="SELECT COUNT(*) FROM $MIGRATION_SCHEMA.job_in_progress")
+JOBS_IN_PROGRESS=$(psql --tuples-only --csv --command="SELECT COUNT(*) FROM $MIGRATION_STATS_SCHEMA.job_in_progress")
 
 if [ "$JOBS_IN_PROGRESS" -eq 0 ]; then
-  echo "No jobs in progress. Running import_from_migration.sql..."
+  echo "No jobs in progress. Running norms migration job..."
+  echo "NORMS_SCHEMA=$NORMS_SCHEMA"
+  echo "MIGRATION_SCHEMA=$MIGRATION_SCHEMA"
   psql --echo-all --variable=NORMS_SCHEMA=$NORMS_SCHEMA --variable=MIGRATION_SCHEMA=$MIGRATION_SCHEMA --file=./import_from_migration.sql
 else
   echo "Jobs are still in progress. Skipping running norms migration job."
